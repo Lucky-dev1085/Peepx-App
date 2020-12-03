8e3a369715c03d5196b191a8f39aa581da846090
Lucky-dev1085 <guerrero.alvaro1085@gmail.com>
2020-12-03 17:45:56 -0500

    change endpoint

--- src/api/Endpoints.js
+++ src/api/Endpoints.js
@@ -1,6 +1,6 @@
 // const BASE_URL = 'http://3.130.226.194/v1';
 // const BASE_URL = 'http://192.168.107.181:8080/v1';
-const BASE_URL = 'http://18.221.54.197/v1';
+const BASE_URL = 'http://3.14.91.112/v1';
 
 export const userEndpoint = `${BASE_URL}/users`;
 export const loginEndpoint = `${BASE_URL}/login`;
